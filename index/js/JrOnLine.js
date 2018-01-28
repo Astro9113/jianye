c48bed8ad7aa0777a23de7ee459748eec4c3c7ed
astro9113 <astro913@163.com>
2018-01-28 14:34:07 +0800

    update

---
+++ index/js/JrOnLine.js
@@ -0,0 +1,78 @@
+document.writeln(" <link href=\"http://www.shsunedu.com/css/telcss.css\" rel=\"stylesheet\">");
+
+
+document.writeln("<script type=\"text/javascript\" charset=\"utf-8\" src=\"http://lead.soperson.com/20003288/10092365.js\"></script>");
+document.writeln("<div>");
+document.writeln("        <div id=\"div1_2\"  style=\" cursor:pointer;width:auto;height:auto;position:relative;right:0px;top:0px;border:0;z-index:999;margin:right;\">");
+document.writeln("       	<div style=\"width:100px;height:auto;float:right;\">");
+document.writeln("    		");
+document.writeln("");
+if ((window.location.href).indexOf("/special")>=0) {
+document.writeln("                       <div style=\"position: fixed; top: 32px; right:10px; width: 160px; height: 800px; background: transparent url(&quot;http://www.shsunedu.com/leyu/tglyk9.jpg&quot;) no-repeat ");
+}else
+{
+document.writeln("                       <div style=\"position: fixed; top: 32px; right:10px; width: 160px; height: 800px; background: transparent url(&quot;http://www.shsunedu.com/leyu/lyk9.jpg&quot;) no-repeat ");
+
+}
+document.writeln("");
+document.writeln("scroll 0% 0%; display: block;\" id=\"Div2_2\">");
+document.writeln("                             <div style=\"left: 8px; top: 133px; width: 135px;margin: 120px 15px 0px; height: 650px;\" id=\"Div3_2\">       ");
+document.writeln("<a style=\"display: block; text-align: center;margin-left: -14px;\" href=\"javascript:void(0)\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><img src=\"http://www.shsunedu.com/leyu/zytzixun.png\"></a>");
+
+document.writeln("                                 <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">上海学校</span></div>       ");
+document.writeln("                                 <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">北京学校</span></div>       ");
+document.writeln("                                 <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">重庆学校</span></div>       ");
+document.writeln("                                 <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">天津学校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">南京学校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">成都分校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">苏州学校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">济南分校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">郑州分校</span></div> ");
+document.writeln("                                 ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">南通学校</span></div>   ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">深圳分校</span></div>    ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">长沙学校</span></div>    ");
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">福州学校</span></div>    ");
+document.writeln("                                 <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">洛阳学校</span></div>       ");
+
+
+
+
+document.writeln("                                    <div class=\"doyoo_panel_cell\" style=\"width:100%;height:20px;line-height:20px;background:url(http://www.shsunedu.com/leyu/zx.jpg) no-repeat left center;margin-top:0px;margin-bottom:0px\" onclick=\"doyoo.util.openChat(\'g=10077992\');return false;\"><span  style=\"color:;font:12px ;height:20px;line-height:20px;margin-left:0px;padding-right:50px;\">企业服务</span></div>    ");
+document.writeln("                             </div>");
+document.writeln("                       </div>");
+document.writeln("");
+document.writeln("        </div>");
+document.writeln("        </div>");
+document.writeln("</div>");
+
+
+document.writeln("<!--集成脚本加载 -->");
+document.writeln("	<script language=\"javascript\" type=\"text/javascript\">");
+document.writeln("	    NTKF_PARAM = {");
+document.writeln("	        siteid: \"kf_9281\",		            //企业ID，为固定值，必填");
+document.writeln("	        settingid: \"g=10069765\",	//接待组ID，为固定值，必填");
+document.writeln("	        uid: \"\",		                //用户ID，未登录可以为空，但不能给null，uid赋予的值显示到小能客户端上");
+document.writeln("	        uname: \"\",		    //用户名，未登录可以为空，但不能给null，uname赋予的值显示到小能客户端上");
+document.writeln("	        isvip: \"0\",                          //是否为vip用户，0代表非会员，1代表会员，取值显示到小能客户端上");
+document.writeln("	        userlevel: \"1\",		                //网站自定义会员级别，0-N，可根据选择判断，取值显示到小能客户端上");
+document.writeln("	        erpparam: \"abc\"                      //erpparam为erp功能的扩展字段，可选，购买erp功能后用于erp功能集成");
+document.writeln("	    }");
+document.writeln("	</script>");
+document.writeln("");
+document.writeln("	<!--基础脚本加载 -->");
+document.writeln("<script type=\"text/javascript\" src=\"http://lead.soperson.com/20003288/10092365.js\" charset=\"utf-8\"></script>");
+document.writeln(" ");
+document.writeln("");
+
+
+function  openChats()
+{
+		doyoo.util.openChat('g=10077992');
+	return false;
+}
--- index/js/OnLine.js
+++ index/js/OnLine.js
@@ -1,2 +1,2 @@
 ﻿//为了兼容老网站上的gb2312代码
-document.writeln("<script type=\"text/javascript\" charset=\"utf-8\"  src=\"http://www.kaisenxue.com/js/JrOnLine.js\"></script>");
+document.writeln("<script type=\"text/javascript\" charset=\"utf-8\"  src=\"/js/JrOnLine.js\"></script>");
